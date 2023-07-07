1eeef5fb4c772f2258b0b9cebeeb2bd7e0eda4b1
RuslanDidun <ruslandidun5@gmail.com>
2023-07-06 19:14:59 -0600

    5kyu

--- JavaScript_6Kyu.js
+++ JavaScript_6Kyu.js
@@ -157,3 +157,22 @@
 
 //   return result;
 // }
+
+// var maxSequence = function (arr) {
+//   let maxSum = 0;
+//   let currentSum = 0;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     currentSum += arr[i];
+
+//     if (currentSum > maxSum) {
+//       maxSum = currentSum;
+//     }
+
+//     if (currentSum < 0) {
+//       currentSum = 0;
+//     }
+//   }
+
+//   return maxSum;
+// };
